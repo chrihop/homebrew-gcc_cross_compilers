065e21112c8a9e443b8e84d4e93f7c0a716a51b4
Hao Chen <hao.chen@yale.edu>
2016-11-17 21:35:48 +0800

    towards 6.2

--- i586-elf-gcc.rb
+++ i586-elf-gcc.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class I586ElfGcc < Formula
   homepage 'http://gcc.gnu.org'
-  url "http://ftpmirror.gnu.org/gcc/gcc-5.2.0/gcc-5.2.0.tar.bz2"
-  mirror "https://ftp.gnu.org/gnu/gcc/gcc-5.2.0/gcc-5.2.0.tar.bz2"
-  sha256 "5f835b04b5f7dd4f4d2dc96190ec1621b8d89f2dc6f638f9f8bc1b1014ba8cad"
+  url "https://mirrors.tuna.tsinghua.edu.cn/gnu/gcc/gcc-6.2.0/gcc-6.2.0.tar.bz2"
+  mirror "http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-6.2.0/gcc-6.2.0.tar.bz2"
+  sha256 "9944589fc722d3e66308c0ce5257788ebd7872982a718aa2516123940671b7c5"
 
   depends_on "gmp"
   depends_on "libmpc"
@@ -16,14 +16,14 @@ class I586ElfGcc < Formula
     binutils = Formula.factory 'i586-elf-binutils'
 
 
-    ENV['CC'] = '/usr/local/opt/gcc/bin/gcc-5'
-    ENV['CXX'] = '/usr/local/opt/gcc/bin/g++-5'
-    ENV['CPP'] = '/usr/local/opt/gcc/bin/cpp-5'
-    ENV['LD'] = '/usr/local/opt/gcc/bin/gcc-5'
+    ENV['CC'] = '/usr/local/opt/gcc/bin/gcc-6'
+    ENV['CXX'] = '/usr/local/opt/gcc/bin/g++-6'
+    ENV['CPP'] = '/usr/local/opt/gcc/bin/cpp-6'
+    ENV['LD'] = '/usr/local/opt/gcc/bin/gcc-6'
     ENV['PATH'] += ":#{binutils.prefix/"bin"}"
 
     mkdir 'build' do
-      system '../configure', '--disable-nls', '--target=i586-elf',
+      system '../configure', '--disable-nls', '--target=i386-elf',
                              '--disable-werror',
                              "--prefix=#{prefix}",
                              "--enable-languages=c",
@@ -33,7 +33,7 @@ class I586ElfGcc < Formula
                              "--with-mpc=#{Formula["libmpc"].opt_prefix}"
       system 'make all-gcc'
       system 'make install-gcc'
-      FileUtils.ln_sf binutils.prefix/"i586-elf", prefix/"i586-elf"
+      FileUtils.ln_sf binutils.prefix/"i386-elf", prefix/"i386-elf"
       system 'make all-target-libgcc'
       system 'make install-target-libgcc'
       FileUtils.rm_rf share/"man"/"man7"
